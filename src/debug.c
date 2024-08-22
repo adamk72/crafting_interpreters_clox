04ea2d97714d3fcef9344a95b0a90d3f0c6e1ecc
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-22 10:12:38 -0500

    chore: savepoint

--- src/debug.c
+++ src/debug.c
@@ -20,7 +20,7 @@ static int simpleInstruction(const char *name, int offset)
 
 int disassembleInstruction(Chunk *chunk, int offset)
 {
-  printf("%04d ", offset);
+  printf("%04d ", offset); // note there's no \n
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction)
