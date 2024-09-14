fbde945afefefd0e112effca48b164bebfb0b771
Adam Kecskes <adam@kecskes.net>
2024-09-14 16:58:30 -0500

    finalize chap 18

--- src/vm.c
+++ src/vm.c
@@ -54,7 +54,8 @@ static Value peek(int distance)
   return vm.stackTop[-1 - distance];
 }
 
-static bool isFalsey(Value value) {
+static bool isFalsey(Value value)
+{
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
 
@@ -107,6 +108,19 @@ static InterpretResult run(void)
       break;
     case OP_FALSE:
       push(BOOL_VAL(false));
+      break;
+    case OP_EQUAL:
+    {
+      Value b = pop();
+      Value a = pop();
+      push(BOOL_VAL(valuesEqual(a, b)));
+      break;
+    }
+    case OP_GREATER:
+      BINARY_OP(BOOL_VAL, >);
+      break;
+    case OP_LESS:
+      BINARY_OP(BOOL_VAL, <);
       break;
     case OP_ADD:
       BINARY_OP(NUMBER_VAL, +);
