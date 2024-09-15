43ec6f69fefc3fce89150bd8b7e14d9f6a24ff67
Adam Kecskes <adam@kecskes.net>
2024-09-14 19:43:35 -0500

    chap 20.5 better string comparison

--- include/vm.h
+++ include/vm.h
@@ -3,6 +3,7 @@
 
 #include "../include/chunk.h"
 #include "../include/value.h"
+#include "../include/table.h"
 
 #define STACK_MAX 256
 
@@ -12,6 +13,7 @@ typedef struct
   uint8_t *ip;
   Value stack[STACK_MAX];
   Value *stackTop;
+  Table strings;
   Obj *objects;
 } VM;
 
