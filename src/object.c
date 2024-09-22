45e333e0454e51085a2d1e64b82a35274ef5e4f5
Adam Kecskes <adam@kecskes.net>
2024-09-22 17:27:05 -0500

    ch24.7 native functions

--- src/object.c
+++ src/object.c
@@ -39,6 +39,13 @@ ObjFunction *newFunction(void)
   return function;
 }
 
+ObjNative *newNative(NativeFn function)
+{
+  ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+  native->function = function;
+  return native;
+}
+
 static uint32_t hashString(const char *key, int length)
 {
   uint32_t hash = 2166136261u;
@@ -93,6 +100,9 @@ void printObject(Value value)
   {
   case OBJ_FUNCTION:
     printFunction(AS_FUNCTION(value));
+    break;
+  case OBJ_NATIVE:
+    printf("<native fn>");
     break;
   case OBJ_STRING:
     printf("%s", AS_CSTRING(value));
--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "../include/common.h"
 #include "../include/compiler.h"
@@ -11,6 +12,11 @@
 #include "../include/vm.h"
 
 VM vm;
+
+static Value clockNative(__attribute__((unused)) int argCount, __attribute__((unused))  Value *args)
+{
+  return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 
 static void resetStack(void)
 {
@@ -46,12 +52,23 @@ static void runtimeError(const char *format, ...)
   resetStack();
 }
 
+static void defineNative(const char *name, NativeFn function)
+{
+  push(OBJ_VAL(copyString(name, (int)strlen(name))));
+  push(OBJ_VAL(newNative(function)));
+  tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+  pop();
+  pop();
+}
+
 void initVM(void)
 {
   resetStack();
   vm.objects = NULL;
   initTable(&vm.globals);
   initTable(&vm.strings);
+
+  defineNative("clock", clockNative);
 }
 
 void freeVM(void)
@@ -108,6 +125,14 @@ static bool callValue(Value callee, int argCount)
     {
     case OBJ_FUNCTION:
       return call(AS_FUNCTION(callee), argCount);
+    case OBJ_NATIVE:
+    {
+      NativeFn native = AS_NATIVE(callee);
+      Value result = native(argCount, vm.stackTop - argCount);
+      vm.stackTop -= argCount + 1;
+      push(result);
+      return true;
+    }
     default:
       break; // Non-callable object type.
     }
