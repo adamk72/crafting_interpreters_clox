efa4bb872b691e7db1eed9b9f8d2a90c9385900c
Adam Kecskes <adam@kecskes.net>
2024-09-22 14:31:10 -0500

    ch22.2 initial scope handling

--- src/compiler.c
+++ src/compiler.c
@@ -188,6 +188,14 @@ static void endCompiler(void)
 #endif
 }
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
 static void expression(void);
 static void statement(void);
 static void declaration(void);
@@ -302,6 +310,14 @@ static void expression(void)
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration(void)
 {
   uint8_t global = parseVariable("Expect variable name.");
@@ -381,6 +397,12 @@ static void statement(void)
   if (match(TOKEN_PRINT))
   {
     printStatement();
+  }
+  else if (match(TOKEN_LEFT_BRACE))
+  {
+    beginScope();
+    block();
+    endScope();
   }
   else
   {
