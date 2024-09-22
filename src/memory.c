04221f3faae96e8759dde9bc7fba12ccc4be723a
Adam Kecskes <adam@kecskes.net>
2024-09-22 16:18:21 -0500

    ch24.1 initial call stack setup

--- src/memory.c
+++ src/memory.c
@@ -3,7 +3,6 @@
 #include "../include/memory.h"
 #include "../include/vm.h"
 
-
 void *reallocate(void *pointer, __attribute__((unused)) size_t oldSize, size_t newSize)
 {
   if (newSize == 0)
@@ -19,21 +18,33 @@ void *reallocate(void *pointer, __attribute__((unused)) size_t oldSize, size_t n
   return result;
 }
 
-static void freeObject(Obj* object) {
-  switch (object->type) {
-    case OBJ_STRING: {
-      ObjString* string = (ObjString*)object;
-      FREE_ARRAY(char, string->chars, string->length + 1);
-      FREE(ObjString, object);
-      break;
-    }
+static void freeObject(Obj *object)
+{
+  switch (object->type)
+  {
+  case OBJ_FUNCTION:
+  {
+    ObjFunction *function = (ObjFunction *)object;
+    freeChunk(&function->chunk);
+    FREE(ObjFunction, object);
+    break;
+  }
+  case OBJ_STRING:
+  {
+    ObjString *string = (ObjString *)object;
+    FREE_ARRAY(char, string->chars, string->length + 1);
+    FREE(ObjString, object);
+    break;
+  }
   }
 }
 
-void freeObjects(void) {
-  Obj* object = vm.objects;
-  while (object != NULL) {
-    Obj* next = object->next;
+void freeObjects(void)
+{
+  Obj *object = vm.objects;
+  while (object != NULL)
+  {
+    Obj *next = object->next;
     freeObject(object);
     object = next;
   }
